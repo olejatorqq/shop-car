ca0d2de656ca5f03e469761f9cb79ee5a584788f
olejatorqq <oleg.orlowsky@gmail.com>
2023-06-18 13:07:08 +0300

    Test commit

---
+++ src/main/java/com/example/shopcar/ShopCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/shopcar/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.shopcar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MainController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
